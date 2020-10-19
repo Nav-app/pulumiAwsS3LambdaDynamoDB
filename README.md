baaf9f47e6fc16f8babb348835277448ada5f235
Nw-App <nkdutt001@gmail.com>
2020-10-19 03:47:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pulumi-aws-s3-lambda-dynamoDB
+ Create and deploy a serverless application that processes uploads to a storage bucket and builds an index of the files in a database table
---
+++ index.ts
@@ -0,0 +1,65 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+
+// Create an s3 bucket to upload a new file.
+const docsBucket = new aws.s3.Bucket("docs");
+
+// Export the name of the bucket.
+export const bucketName = docsBucket.id;
+
+// Configure IAM so that the AWS Lambda has DynamoDB access.
+const docsHandlerRole = new aws.iam.Role("docsHandlerRole", {
+   assumeRolePolicy: {
+      Version: "2012-10-17",
+      Statement: [{
+         Action: "sts:AssumeRole",
+         Principal: {
+            Service: "lambda.amazonaws.com",
+         },
+         Effect: "Allow",
+         Sid: "",
+      }],
+   },
+});
+new aws.iam.RolePolicyAttachment("writeObjectDetails", {
+   role: docsHandlerRole,
+   policyArn: aws.iam.ManagedPolicies.AmazonDynamoDBFullAccess,
+});
+
+// Create a DynamoDB Table
+const db = new aws.dynamodb.Table("newtable", {
+    attributes: [
+        { name: "Id", type: "S" },
+    ],
+    hashKey: "Id",
+    readCapacity: 10,
+    writeCapacity: 10,
+});
+
+// Create a lambda function and add a python file.
+const pythonLambda = new aws.lambda.Function("aws-lambda-dynamodb-python", {
+    runtime: aws.lambda.Python3d6Runtime,
+    code: new pulumi.asset.AssetArchive({
+        ".": new pulumi.asset.FileArchive("./python"),
+    }),
+    timeout: 5,
+    handler: "lambda_function.lambda_handler",
+    role: docsHandlerRole.arn
+});
+
+// Give the bucket permission to invoke the lambda.
+const permission = new aws.lambda.Permission("invokelambda", {
+  function: pythonLambda,
+  action: "lambda:InvokeFunction",
+  principal: "s3.amazonaws.com", sourceArn: docsBucket.id.apply(bucketName => `arn:aws:s3:::${bucketName}`), });
+
+  // Now hookup a notification that will trigger the lambda when any object is created in the bucket.
+const notification = new aws.s3.BucketNotification("onAnyObjectCreated", {
+      bucket: docsBucket.id,
+      lambdaFunctions: [{
+          events: ["s3:ObjectCreated:*"],
+          lambdaFunctionArn: pythonLambda.arn,
+      }],
+  })
